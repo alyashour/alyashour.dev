037579540164b55c919fa7866bf4925faa6dc24c
Aly Ashour <alyashour1@gmail.com>
2025-05-29 02:13:51 -0400

    added base, up to dino game + basic css

---
+++ README.md
@@ -0,0 +1,4 @@
+# Aly Ashour
+This is my personal website.
+I am not a web developer so this is particularly function over form.
+
---
+++ js/load_projects.js
@@ -0,0 +1,12 @@
+class Project {
+    constructor(name, date, github_link, img_path) {
+        this.name = name;
+        this.date = date;
+        this.github_link = github_link;
+        this.img_path = img_path;
+    }
+
+    createHTMLElement() {
+        return <p>Zouie mama</p>;
+    }
+}
