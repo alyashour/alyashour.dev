dc97c7a6771c9e91a46cf1909783377d6a5b0343
Aly Ashour <alyashour1@gmail.com>
2025-06-07 14:43:04 -0400

    added some subpages and links

---
+++ js/asteroids-game.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('asteroids-game');
+
+if (container == null) {
+    console.log('Failed to initialize asteroids game, container is null');
+}
+
--- js/dino-game.js
+++
@@ -1,6 +0,0 @@
-const container = document.getElementById('dino-game');
-
-if (container == null) {
-    console.log('Failed to initialize dino game, container is null');
-}
-
