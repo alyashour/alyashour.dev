bc9e2827b03c90f2c1e7c0c557c022933a814c46
Aly Ashour <alyashour1@gmail.com>
2025-09-28 20:20:43 -0400

    astro initial commit

--- js/scroll-restoration.js
+++
@@ -1,7 +0,0 @@
-function saveScrollPosition() {
-  const route = getCurrentRoute();
-  sessionStorage.setItem(`scroll-${route}`, window.scrollY);
-}
-
-window.addEventListener("hashchange", saveScrollPosition);
-window.addEventListener("pagehide", saveScrollPosition); // For YouTube-style nav
